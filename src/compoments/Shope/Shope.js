7f2da14293f8687ce95672f5de7a266fb88ea04c
Rafsan Ahmed Raj <rafsan.web.2022@gmail.com>
2022-03-25 17:43:44 +0600

    first commit

---
+++ src/compoments/Shope/Shope.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shope.css'
+
+const Shope = () => {
+    const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data => setProduct(data))
+
+    },[])
+
+    const handleAddToCart =(product)=>{
+      console.log(product)
+      const newCart = [...cart, product];
+      setCart(newCart)
+
+    }
+    
+
+    return (
+        <div className='shop-container'>
+          <div className="products-container">
+            {
+                products.map(product=><Product key={product.id} 
+                product={product}
+                handleAddToCart={handleAddToCart}
+                
+                >
+
+                </Product>)
+            }
+              
+          </div>
+
+          <div className="cart-container">
+            <h2>Order Summary</h2>
+            <p>Selected Items: {cart.length} </p>
+             
+
+          </div>
+        </div>
+    );
+};
+
+export default Shope;
